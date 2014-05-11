127baa54ee26c962279f18d8d2d52c1a80cbf79d
Martin Chudik <mchudik@echo360.com>
2014-05-11 18:12:00 -0400

    Added Drawing to Slide and Multi-choice items

--- ALPPaneMultipleChoice.Designer.cs
+++ ALPPaneMultipleChoice.Designer.cs
@@ -28,19 +28,223 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.QuestionLabel = new System.Windows.Forms.Label();
+            this.QuestionTextBox = new System.Windows.Forms.TextBox();
+            this.AnswersLabel = new System.Windows.Forms.Label();
+            this.JustificationTextBox = new System.Windows.Forms.TextBox();
+            this.JustificationLabel = new System.Windows.Forms.Label();
+            this.AddJustificationCheckBox = new System.Windows.Forms.CheckBox();
+            this.AnswerDescTextBox = new System.Windows.Forms.TextBox();
+            this.JustificationDescTextBox = new System.Windows.Forms.TextBox();
+            this.ALabel = new System.Windows.Forms.Label();
+            this.AButton = new System.Windows.Forms.Button();
+            this.BButton = new System.Windows.Forms.Button();
+            this.BLabel = new System.Windows.Forms.Label();
+            this.CButton = new System.Windows.Forms.Button();
+            this.CLabel = new System.Windows.Forms.Label();
+            this.DButton = new System.Windows.Forms.Button();
+            this.DLabel = new System.Windows.Forms.Label();
             this.SuspendLayout();
+            // 
+            // QuestionLabel
+            // 
+            this.QuestionLabel.AutoSize = true;
+            this.QuestionLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.QuestionLabel.Location = new System.Drawing.Point(18, 13);
+            this.QuestionLabel.Name = "QuestionLabel";
+            this.QuestionLabel.Size = new System.Drawing.Size(68, 18);
+            this.QuestionLabel.TabIndex = 3;
+            this.QuestionLabel.Text = "Question";
+            // 
+            // QuestionTextBox
+            // 
+            this.QuestionTextBox.Location = new System.Drawing.Point(21, 39);
+            this.QuestionTextBox.Multiline = true;
+            this.QuestionTextBox.Name = "QuestionTextBox";
+            this.QuestionTextBox.Size = new System.Drawing.Size(219, 69);
+            this.QuestionTextBox.TabIndex = 4;
+            this.QuestionTextBox.Text = "Define Question";
+            // 
+            // AnswersLabel
+            // 
+            this.AnswersLabel.AutoSize = true;
+            this.AnswersLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.AnswersLabel.Location = new System.Drawing.Point(18, 118);
+            this.AnswersLabel.Name = "AnswersLabel";
+            this.AnswersLabel.Size = new System.Drawing.Size(65, 18);
+            this.AnswersLabel.TabIndex = 5;
+            this.AnswersLabel.Text = "Answers";
+            // 
+            // JustificationTextBox
+            // 
+            this.JustificationTextBox.Location = new System.Drawing.Point(21, 408);
+            this.JustificationTextBox.Multiline = true;
+            this.JustificationTextBox.Name = "JustificationTextBox";
+            this.JustificationTextBox.Size = new System.Drawing.Size(219, 69);
+            this.JustificationTextBox.TabIndex = 7;
+            this.JustificationTextBox.Text = "Briefly explain your answer.";
+            // 
+            // JustificationLabel
+            // 
+            this.JustificationLabel.AutoSize = true;
+            this.JustificationLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.JustificationLabel.Location = new System.Drawing.Point(18, 322);
+            this.JustificationLabel.Name = "JustificationLabel";
+            this.JustificationLabel.Size = new System.Drawing.Size(86, 18);
+            this.JustificationLabel.TabIndex = 6;
+            this.JustificationLabel.Text = "Justification";
+            // 
+            // AddJustificationCheckBox
+            // 
+            this.AddJustificationCheckBox.AutoSize = true;
+            this.AddJustificationCheckBox.Location = new System.Drawing.Point(21, 343);
+            this.AddJustificationCheckBox.Name = "AddJustificationCheckBox";
+            this.AddJustificationCheckBox.Size = new System.Drawing.Size(133, 21);
+            this.AddJustificationCheckBox.TabIndex = 8;
+            this.AddJustificationCheckBox.Text = "Add Justification";
+            this.AddJustificationCheckBox.UseVisualStyleBackColor = true;
+            // 
+            // AnswerDescTextBox
+            // 
+            this.AnswerDescTextBox.BackColor = System.Drawing.SystemColors.ControlLight;
+            this.AnswerDescTextBox.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.AnswerDescTextBox.Enabled = false;
+            this.AnswerDescTextBox.Location = new System.Drawing.Point(21, 139);
+            this.AnswerDescTextBox.Multiline = true;
+            this.AnswerDescTextBox.Name = "AnswerDescTextBox";
+            this.AnswerDescTextBox.Size = new System.Drawing.Size(219, 41);
+            this.AnswerDescTextBox.TabIndex = 9;
+            this.AnswerDescTextBox.Text = "You can select more than one correct answer.";
+            // 
+            // JustificationDescTextBox
+            // 
+            this.JustificationDescTextBox.BackColor = System.Drawing.SystemColors.ControlLight;
+            this.JustificationDescTextBox.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.JustificationDescTextBox.Enabled = false;
+            this.JustificationDescTextBox.Location = new System.Drawing.Point(21, 366);
+            this.JustificationDescTextBox.Multiline = true;
+            this.JustificationDescTextBox.Name = "JustificationDescTextBox";
+            this.JustificationDescTextBox.Size = new System.Drawing.Size(207, 41);
+            this.JustificationDescTextBox.TabIndex = 10;
+            this.JustificationDescTextBox.Text = "Check this box to ask students to add a follow up free response.";
+            // 
+            // ALabel
+            // 
+            this.ALabel.AutoSize = true;
+            this.ALabel.Location = new System.Drawing.Point(37, 189);
+            this.ALabel.Name = "ALabel";
+            this.ALabel.Size = new System.Drawing.Size(17, 17);
+            this.ALabel.TabIndex = 11;
+            this.ALabel.Text = "A";
+            // 
+            // AButton
+            // 
+            this.AButton.Location = new System.Drawing.Point(58, 184);
+            this.AButton.Name = "AButton";
+            this.AButton.Size = new System.Drawing.Size(65, 26);
+            this.AButton.TabIndex = 12;
+            this.AButton.Text = "Correct";
+            this.AButton.UseVisualStyleBackColor = true;
+            // 
+            // BButton
+            // 
+            this.BButton.Location = new System.Drawing.Point(58, 215);
+            this.BButton.Name = "BButton";
+            this.BButton.Size = new System.Drawing.Size(65, 26);
+            this.BButton.TabIndex = 14;
+            this.BButton.Text = "Correct";
+            this.BButton.UseVisualStyleBackColor = true;
+            // 
+            // BLabel
+            // 
+            this.BLabel.AutoSize = true;
+            this.BLabel.Location = new System.Drawing.Point(37, 220);
+            this.BLabel.Name = "BLabel";
+            this.BLabel.Size = new System.Drawing.Size(17, 17);
+            this.BLabel.TabIndex = 13;
+            this.BLabel.Text = "B";
+            // 
+            // CButton
+            // 
+            this.CButton.Location = new System.Drawing.Point(58, 246);
+            this.CButton.Name = "CButton";
+            this.CButton.Size = new System.Drawing.Size(65, 26);
+            this.CButton.TabIndex = 16;
+            this.CButton.Text = "Correct";
+            this.CButton.UseVisualStyleBackColor = true;
+            // 
+            // CLabel
+            // 
+            this.CLabel.AutoSize = true;
+            this.CLabel.Location = new System.Drawing.Point(37, 251);
+            this.CLabel.Name = "CLabel";
+            this.CLabel.Size = new System.Drawing.Size(17, 17);
+            this.CLabel.TabIndex = 15;
+            this.CLabel.Text = "C";
+            // 
+            // DButton
+            // 
+            this.DButton.Location = new System.Drawing.Point(58, 278);
+            this.DButton.Name = "DButton";
+            this.DButton.Size = new System.Drawing.Size(65, 26);
+            this.DButton.TabIndex = 18;
+            this.DButton.Text = "Correct";
+            this.DButton.UseVisualStyleBackColor = true;
+            // 
+            // DLabel
+            // 
+            this.DLabel.AutoSize = true;
+            this.DLabel.Location = new System.Drawing.Point(37, 283);
+            this.DLabel.Name = "DLabel";
+            this.DLabel.Size = new System.Drawing.Size(18, 17);
+            this.DLabel.TabIndex = 17;
+            this.DLabel.Text = "D";
             // 
             // ALPPaneMultipleChoice
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.SystemColors.ControlLight;
+            this.Controls.Add(this.DButton);
+            this.Controls.Add(this.DLabel);
+            this.Controls.Add(this.CButton);
+            this.Controls.Add(this.CLabel);
+            this.Controls.Add(this.BButton);
+            this.Controls.Add(this.BLabel);
+            this.Controls.Add(this.AButton);
+            this.Controls.Add(this.ALabel);
+            this.Controls.Add(this.JustificationDescTextBox);
+            this.Controls.Add(this.AnswerDescTextBox);
+            this.Controls.Add(this.AddJustificationCheckBox);
+            this.Controls.Add(this.JustificationTextBox);
+            this.Controls.Add(this.JustificationLabel);
+            this.Controls.Add(this.AnswersLabel);
+            this.Controls.Add(this.QuestionTextBox);
+            this.Controls.Add(this.QuestionLabel);
             this.Name = "ALPPaneMultipleChoice";
-            this.Size = new System.Drawing.Size(278, 470);
+            this.Size = new System.Drawing.Size(278, 502);
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
+
+        private System.Windows.Forms.Label QuestionLabel;
+        private System.Windows.Forms.TextBox QuestionTextBox;
+        private System.Windows.Forms.Label AnswersLabel;
+        private System.Windows.Forms.TextBox JustificationTextBox;
+        private System.Windows.Forms.Label JustificationLabel;
+        private System.Windows.Forms.CheckBox AddJustificationCheckBox;
+        private System.Windows.Forms.TextBox AnswerDescTextBox;
+        private System.Windows.Forms.TextBox JustificationDescTextBox;
+        private System.Windows.Forms.Label ALabel;
+        private System.Windows.Forms.Button AButton;
+        private System.Windows.Forms.Button BButton;
+        private System.Windows.Forms.Label BLabel;
+        private System.Windows.Forms.Button CButton;
+        private System.Windows.Forms.Label CLabel;
+        private System.Windows.Forms.Button DButton;
+        private System.Windows.Forms.Label DLabel;
     }
 }
--- ALPRibbon.cs
+++ ALPRibbon.cs
@@ -51,8 +51,32 @@ namespace ALPRibbon
                 PowerPoint.View oView = oApp.ActiveWindow.View;
                 
                 // Insert Slide after the current slide and select it
-                oPres.Slides.Add(RibbonAddIn.ALPCurrentSlide + 1, oLayout);
-                oView.GotoSlide(RibbonAddIn.ALPCurrentSlide + 1);
+                PowerPoint.Slide oSlide = oPres.Slides.Add(RibbonAddIn.ALPCurrentSlide + 1, oLayout);
+                oView.GotoSlide(oSlide.SlideIndex);
+
+                // Display Question Title
+                PowerPoint.PageSetup oPageSetup = oPres.PageSetup;
+                float nSlideWidth = oPageSetup.SlideWidth;
+                float nSlideHeight = oPageSetup.SlideHeight;
+                PowerPoint.Shapes oShapes = oSlide.Shapes;
+                PowerPoint.Shape oShapeArt = oShapes.AddTextEffect(Microsoft.Office.Core.MsoPresetTextEffect.msoTextEffect16, "Define Question", "Tahoma", 42, Microsoft.Office.Core.MsoTriState.msoFalse, Microsoft.Office.Core.MsoTriState.msoFalse, 100, 100);
+                oShapeArt.Left = (nSlideWidth - oShapeArt.Width) / 2;
+                oShapeArt.Top = (nSlideHeight - oShapeArt.Height) / 7;
+
+                // Display bulleted answer list
+                PowerPoint.Shape oShapeText = oShapes.AddTextbox(Microsoft.Office.Core.MsoTextOrientation.msoTextOrientationHorizontal, 100, 100, 500, 500);
+                PowerPoint.TextRange oTextRange = oShapeText.TextFrame.TextRange;
+                oTextRange.Text = "Answer One\nAnswer Two\nAnswer Three\nAnswer Four";
+                oTextRange.ParagraphFormat.Alignment = PowerPoint.PpParagraphAlignment.ppAlignLeft;
+                PowerPoint.ParagraphFormat oParagraphFormat = oTextRange.ParagraphFormat;
+                oParagraphFormat.SpaceWithin = 2;
+                oParagraphFormat.Bullet.Type = PowerPoint.PpBulletType.ppBulletNumbered;
+                oTextRange.Font.Name = "Tahoma";
+                oTextRange.Font.Size = 24;
+                oShapeText.Width = 8*(nSlideWidth / 10);
+                oShapeText.Height = oShapeText.TextFrame.TextRange.BoundHeight;
+                oShapeText.Left = nSlideWidth / 10;
+                oShapeText.Top = 3*(nSlideHeight / 9);
             }
         }
     }
